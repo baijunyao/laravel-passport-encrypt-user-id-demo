3a04beb2d55993357a8148ace24aff9fc8722939
baijunyao <baijunyao@baijunyao.com>
2019-01-11 22:04:07 +0800

    修复不严谨的地方

--- app/OauthAuthCode.php
+++ app/OauthAuthCode.php
@@ -8,7 +8,8 @@ use Laravel\Passport\AuthCode;
 class OauthAuthCode extends AuthCode
 {
     /**
-     * setUserIdAttribute() does not work, Need to override the parent method.
+     * 因为 setRawAttributes 不能触发 setUserIdAttribute()
+     * 所以此处需要覆盖父级的 setRawAttributes()
      *
      * @param array $attributes
      * @param bool  $sync
@@ -21,8 +22,10 @@ class OauthAuthCode extends AuthCode
          * Decrypt user_id
          *
          * Encrypt user_id in @see \App\Http\Controllers\Oauth\AuthorizationController::authorize()
+         * 因为模型的 get() 方法会触发 setRawAttributes ；虽然 Passport 没有使用模型获取数据
+         * 但是为了严谨此处需要增加 $this->exists 的判断； 只在存储的时候解密 user_id
          */
-        if (isset($attributes['user_id'])) {
+        if (isset($attributes['user_id']) && !$this->exists) {
             $attributes['user_id'] = current(Hashids::decode($attributes['user_id']));
         }
 
